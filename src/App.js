2595dd81ba4bc91523365eda57a480210cec3c26
Nithupm <nithu.zugzwang@gmail.com>
2023-06-08 12:58:15 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Scheduler from './components/Scheduler';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Scheduler/>
+  
+    </div>
+  );
+}
+
+export default App;
